0be4bf7c2e91cd2b6bc56b6dee72032e0833f110
aleksey ignatyev <a.ignatyev@smartcat.pro>
2017-02-18 19:26:41 +0300

    second type

--- ViewBot/Program.cs
+++ ViewBot/Program.cs
@@ -9,8 +9,8 @@ using System.Threading;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using NLog;
-using xNet;
-using HttpMethod = xNet.HttpMethod;
+using Extreme.Net;
+using System.Net;
 
 namespace ViewBot
 {
@@ -34,12 +34,15 @@ namespace ViewBot
 			var opt = new ParallelOptions();
 			opt.MaxDegreeOfParallelism = 1000;
 
-			Parallel.For(0, _list.Count, opt, ConnectToStreamer);
+			//Parallel.For(0, _list.Count, opt, ConnectToStreamer);
 
 			//	Parallel.ForEach(list, ConnectToStreamer);
 
-			//for (int i = 0; i < _list.Count; i++)
-			//{
+			for (int i = 0; i < _list.Count; i++)
+			{
+				ThreadPool.QueueUserWorkItem(ConnectToStreamer, i);
+			}
+
 			//	var task = new Thread(ConnectToStreamer);
 				
 			//	try
@@ -57,14 +60,17 @@ namespace ViewBot
 			Console.Read();
 		}
 
-		public static void ConnectToStreamer(int i)
+		public async static void ConnectToStreamer(object j)
 		{
+			int i = (int)j;
+
+			var client = _list[(int)i];
 
 			var livestreamerPath = $"{ConfigurationManager.AppSettings["LivestreamerPath"]}livestreamer.exe";
 
 			Process cmd = new Process();
 			cmd.StartInfo.FileName = livestreamerPath;
-			cmd.StartInfo.Arguments = $"--http-header Client-ID=m35gcmzfmcpybvz54e2j8iaz2phxxod {_streamName} -j";
+			cmd.StartInfo.Arguments = $"--http-proxy http://{client.Host}:{client.Port} --http-header Client-ID=m35gcmzfmcpybvz54e2j8iaz2phxxod {_streamName} -j";
 
 			cmd.StartInfo.RedirectStandardInput = true;
 			cmd.StartInfo.RedirectStandardOutput = true;
@@ -74,16 +80,57 @@ namespace ViewBot
 
 			StreamData ob = JsonConvert.DeserializeObject<StreamData>(cmd.StandardOutput.ReadToEnd());
 
-			_url = ob.Streams.Audio.Url;
+			string url;
+			try
+			{
+				url = ob.Streams.Audio.Url;
+			}
+
+			catch(Exception)
+			{
+				cmd.Dispose();
+				return;
+			}
+			cmd.Dispose();
+
+			Stopwatch sw = new Stopwatch();
+
+			sw.Start();
 
 			while (true)
 			{
-				using (var request = new HttpRequest())
+				if (sw.Elapsed.Seconds > 55)
 				{
-					var client = _list[(int)i];
+					cmd = new Process();
+					cmd.StartInfo.FileName = livestreamerPath;
+					cmd.StartInfo.Arguments = $"--http-proxy http://{client.Host}:{client.Port} --http-header Client-ID=m35gcmzfmcpybvz54e2j8iaz2phxxod {_streamName} -j";
 
-					request.Proxy = client;
+					cmd.StartInfo.RedirectStandardInput = true;
+					cmd.StartInfo.RedirectStandardOutput = true;
+					cmd.StartInfo.CreateNoWindow = true;
+					cmd.StartInfo.UseShellExecute = false;
+					cmd.Start();
 
+					ob = JsonConvert.DeserializeObject<StreamData>(cmd.StandardOutput.ReadToEnd());
+										
+					try
+					{
+						url = ob.Streams.Audio.Url;
+					}
+
+					catch (Exception)
+					{
+						cmd.Dispose();
+						return;
+					}
+					cmd.Dispose();
+					sw.Reset();
+				}
+
+				ProxyHandler handler = new ProxyHandler(client);
+
+				using (var request = new HttpClient(handler))
+				{
 					//HttpRequestMessage requests = new HttpRequestMessage();
 					//requests.RequestUri = new Uri(ob.Streams.Audio.Url);
 					//requests.Method = System.Net.Http.HttpMethod.Get;
@@ -95,21 +142,29 @@ namespace ViewBot
 					//};
 
 					//HttpClient cl = new HttpClient(hand);
-					
-					HttpResponse res;
+
+					string res;
 
 					try
 					{
 						//res = await cl.SendAsync(requests);
-						res = request.Get(_url);
+						res = await request.GetStringAsync(url);
+
+						var firstPart = url.Split(new string[] { "index" }, StringSplitOptions.None)[0];
+						var secondPart = res.Split('#')[14].Split(new string [] { "index" }, StringSplitOptions.None)[1];
+						var newUrl = firstPart + "index" + secondPart;
+
+						res = await request.GetStringAsync(newUrl);
+
+						Console.WriteLine($"Отправлено {client.Type} - {client.Host}");
 					}
 					catch (Exception ex)
 					{
 						//	//_logger.Trace(ex);
-						return;
+						//return;
 					}
 
-					Console.WriteLine($"Отправлено {client.Type} - {client.Host}");
+					
 				}
 				
 				Thread.Sleep(5000);
